2c21efc5ff4389f262232a8b9b80866aaa728af8
Crularson <lar24046@byui.edu>
2026-01-15 23:23:40 -0700

    changed JS to change HTML text.

--- w02/ponder/dom-theme.js
+++ w02/ponder/dom-theme.js
@@ -5,16 +5,22 @@ let pageContent = document.querySelector('body');
 selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
+    const contentDiv = document.querySelector('.content');
+    const paragraph = contentDiv.querySelector('p');
     let current = selectElem.value;
     if (current === 'ocean') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
         pageContent.style.fontFamily = "Papyrus, fantasy";
+        paragraph.textContent = 'You have chosen ocean!';
     } else if (current === 'forest') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
         pageContent.style.fontFamily = "Impact, sans-serif";
+        paragraph.textContent = 'You have chosen forest!';
     } else if (current === 'desert') {
         document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
         pageContent.style.fontFamily = "'Big Caslon', serif";
+        paragraph.textContent = 'You have chosen desert!';
+        
     } else {
         // default
         document.body.style.backgroundImage = "none";
